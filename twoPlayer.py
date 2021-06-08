d6e8db020f06a929f6f5a362db67f902dcaf33f5
masonhorder <masonhorder@gmail.com>
2021-06-07 22:07:37 -0700

    working on game physics

--- twoPlayer.py
+++ twoPlayer.py
@@ -1,10 +1,84 @@
 board = [[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],]
 
-
 def printBoard():
   for row in board:
     print(row)
 
+def getGameStatus():
+  gameStatus = 0 # 0 = active game; 1 = player 1 wins; 2 = player 2 wins; 3 = tie game
+
+
+
+
+  # check if game is tie
+  hasZero = False
+  for row in board:
+    for spot in row:
+      if spot == 0:
+        hasZero = True
+  
+  if hasZero == False:
+    gameStatus = 3
+    return gameStatus
+
+  
+
+
+  return gameStatus #no one has won or tied
+        
+
+
+
+def placeMarker(player):
+  gameStatus = getGameStatus() # check if the game has been won or tied
+  
+  if gameStatus == 1:
+    print("congrats player 1 won!")
+    return None
+  
+  elif gameStatus == 2:
+    print("congrats player 2 won!")
+    return None
+  
+  elif gameStatus == 3:
+    print("tie :(")
+    return None
+
+  printBoard()
+  newPlayer = 0
+  spot = int(input("Player " + str(player) + " choose a column(1-7): "))
+
+  # find what height to place the marker at
+  def getHeightOfSpot(spot, height):
+    # print("current occupation: " + str(board[height][spot-1]))
+    if board[height][spot-1] == 0:
+      if height < 0:
+        return None # return an error if the marker cannot be place in this column
+      return height # return the coorect height to place marker
+    else: 
+      getHeightOfSpot(spot, height-1) # move on to check the next available height
+  
+  height = getHeightOfSpot(spot-1, 5) # start checking for the height
+
+  if height == None:
+    print("Column Already Fully Occupied")
+    placeMarker(player) # restart trun as current player
+
+
+  # print("location: " + str(height) + ", " + str(spot-1))
+  board[height][spot-1] = player # place the marker
+
+
+  # find next player
+  if player == 2:
+    newPlayer = 1
+  else:
+    newPlayer = 2
+  placeMarker(newPlayer)
+
+
+
+
 
 print("Welcome to connect four, two player edition")
-printBoard()
+placeMarker(1) # starts game
