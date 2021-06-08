6e5487800468702df2cb77c2c354201581b12ed1
masonhorder <masonhorder@gmail.com>
2021-06-07 21:19:05 -0700

    first commit

---
+++ twoPlayer.py
@@ -0,0 +1,10 @@
+board = [[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],]
+
+
+def printBoard():
+  for row in board:
+    print(row)
+
+
+print("Welcome to connect four, two player edition")
+printBoard()
