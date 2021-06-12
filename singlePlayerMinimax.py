70b2793d6710041b43e0b5f0cb3b225ba0fc4285
masonhorder <masonhorder@gmail.com>
2021-06-11 21:46:15 -0700

    good ai

--- singlePlayerMinimax.py
+++ singlePlayerMinimax.py
@@ -22,6 +22,15 @@ def isBoardFull(inputBoard):
   else: 
     return True
   
+
+def getValidMoves(inputBoard):
+  validMoves = []
+  for column in range(7):
+    if getRow(column, 5, inputBoard) != None:
+      validMoves.append([getRow(column, 5, inputBoard), column])
+
+  return validMoves
+
 # check for a streak at a certain length
 def getStreak(inputBoard, streakLength, player):
   streakCount = 0
@@ -113,41 +122,61 @@ def getStreak(inputBoard, streakLength, player):
       
 
      
-
-
-
-def getScore(inputBoard, column, inverseBoard):
+def evaluateSection(section):
   score = 0
 
-  if column == 4:
-    score += 3
-  elif column == 3 or column == 5:
-    score += 1
-  
-  # score is a win for bot or blocks opponent
-  win = 0
-  currentSetStreak = 4
-  while currentSetStreak <= 7:
-    if getStreak(inverseBoard, currentSetStreak, 1) > 0:
-      win = 1
-    if getStreak(inputBoard, currentSetStreak, 2) > 0:
-      win = 2
-    currentSetStreak += 1
-
-  if win == 1:
+  if section.count(2) == 4: # bot winning
     score += 100
-  elif win == 2:
-    score +=1000
 
-  score+=getStreak(inputBoard, 3, 2)*6
-  score+=getStreak(inverseBoard, 3, 1)*4
-  score+=getStreak(inputBoard, 2, 2)*3
-  score+=getStreak(inverseBoard, 3, 1)*1
+  elif section.count(1) == 3 and section.count(0) == 1: # human being set up to win
+    score -= 25
+
+  elif section.count(2) == 3 and section.count(0) == 1:
+    score += 5
+
+  elif section.count(2) == 2 and section.count(0) == 2:
+    score += 2
+
+  return score
+
+def getScore(inputBoard):
+  score = 0
+
+  # Score center column
+  centerColumnArray = []
+  for row in inputBoard:
+    centerColumnArray.append(row[3])
+
+  score += centerColumnArray.count(2) * 3
+
+  for column in range(7):
+    columnArray = []
+    for rowList in inputBoard:
+      columnArray.append(rowList[column])
+    for row in range(6-3):
+      section = columnArray[row:row+4]
+      score += evaluateSection(section)
+      
+  for row in inputBoard:
+    for column in range(7-3):
+      section = row[column:column+4]
+      score += evaluateSection(section)
+
+  for r in range(6-3):
+    for c in range(7-3):
+      section = [inputBoard[r+i][c+i] for i in range(4)]
+      score += evaluateSection(section)
+
+  for r in range(6-3):
+    for c in range(7-3):
+      section = [inputBoard[r+3-i][c+i] for i in range(4)]
+      score += evaluateSection(section)
 
   return score
 
 
 
+  
 
           
 
@@ -183,6 +212,8 @@ def printBoard():
     print(rowString)
   print("\033[93m-------------------\033[0m")
 
+
+
 def getRow(column, inputRow, inputBoard):
   if inputRow < 0:
     return None # return an error if the marker cannot be place in this column
@@ -190,6 +221,8 @@ def getRow(column, inputRow, inputBoard):
     return inputRow # return the correct row to place marker
   else: 
     return getRow(column, inputRow-1, inputBoard) # move on to check the next available row
+
+
 
 # get player 1 move location
 def askPlayerColumn():
@@ -221,6 +254,41 @@ def getDepth(inputBoard):
         movesLeft +=1
 
   return movesLeft
+
+
+def minimax(inputBoard, player, depth):
+  if player == 2:
+    best = [None, None, -infinity]
+  else:
+    best = [None, None, +infinity]
+
+  if getGameStatus(inputBoard) != 0:
+    if getGameStatus(inputBoard) == 3:
+      return [None, None, 0]
+    elif getGameStatus(inputBoard) == 2:
+      return [None, None, 10000000000000]
+    elif getGameStatus(inputBoard) == 2:
+      return [None, None, -10000000000000]
+  
+  if depth == 0:
+    return [None, None, getScore(inputBoard)]
+
+  for move in getValidMoves(inputBoard):
+    newBoard = copy.deepcopy(inputBoard)
+    newBoard[move[0]][move[1]] = player
+    score = minimax(newBoard, nextPlayer(player), depth-1)
+    score[0], score[1] = move[0], move[1]
+
+    if player == 2:
+      if score[2] > best[2]:
+        best = score
+    else:
+      if score[2] < best[2]:
+        best = score
+
+  return best
+
+
 
 
 #####################
@@ -265,38 +333,10 @@ def makeMove(player, message):
   # bots turn to move
   if player == 2:
     startTime = time.time()
-    def minimax(inputBoard, player, column, inverseBoard, depth):
-      if player == 2:
-        best = [-1, -1, -infinity]
-      else:
-        best = [-1, -1, +infinity]
-
-      if getGameStatus(inputBoard) != 0 or depth == 0:
-        score = getScore(inputBoard, column, inverseBoard)
-        return [-1, -1, score]
-
-      for potentialColumn in range(7):
-        potentialRow = getRow(potentialColumn, 5, inputBoard)
-        if potentialRow != None:
-          newBoard = copy.deepcopy(inputBoard)
-          newBoard[potentialRow][potentialColumn] = player
-          inverseBoard = copy.deepcopy(inputBoard)
-          inverseBoard[potentialRow][potentialColumn] = nextPlayer(player)
-          score = minimax(newBoard, nextPlayer(player), potentialColumn, inverseBoard, depth-1)
-          score[0], score[1] = potentialRow, potentialColumn
-
-          if player == 2:
-            if score[2] > best[2]:
-              best = score
-          else:
-            if score[2] < best[2]:
-              best = score
-
-      return best
   
-    move = minimax(board, 2, 0, board, 3)
+    move = minimax(board, 2, 5)
     endTime = time.time()
-    print(endTime-startTime)
+    print("time to make move(seconds):" + str(endTime-startTime))
     # getDepth(board)
     row = move[0]
     column = move[1]
@@ -306,4 +346,5 @@ def makeMove(player, message):
   makeMove(nextPlayer(player), "")
   
 
-makeMove(random.randint(1,2), "\033[93mWelcome to connect four, single player edition\033[0m")
+makeMove(1, "\033[93mWelcome to connect four, single player edition\033[0m")
+# random.randint(1,2)
